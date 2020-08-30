a1be0bd4f5f9776c0e24d0d901c221c889206553
Tobias van Dyk <TobiasJVanDyk@gmail.com>
2020-08-30 09:59:47 +0200

    Add files via upload

---
+++ wm8960-1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/waveshare/WM8960-Audio-HAT
+cd WM8960-Audio-HAT
+sudo ./install.sh
+sudo reboot
---
+++ wm8960-2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo alsamixer
+sudo nano /usr/share/alsa/alsa.conf
+sudo alsamixer
